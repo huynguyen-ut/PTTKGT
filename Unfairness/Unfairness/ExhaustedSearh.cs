5f8f6fad7bfc58f35d3e9572fa42680fffb4e56c
huyite <huyite.vn@gmail.com>
2016-10-24 07:21:38 +0700

    Delete maxmin

---
+++ Unfairness/Unfairness/ExhaustedSearh.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Unfairness
+{
+    class ExhaustedSearh
+    {
+        private int[] K;
+        private int[] N;
+
+       
+
+        public ExhaustedSearh(int[] arr, int k) {
+            N = arr;
+            K = new int[k];
+
+        }
+       
+        public void Try(int i) {
+            for (int index =0; index < N.Count<int>(); index++) {
+                if ( i == 0)
+                {
+                    K[i] = N[index];
+                    Try(i + 1);
+                }
+                else {
+                    if (N[index] > K[i - 1]) {
+                        K[i] = N[index];
+                        if (i == K.Count<int>() - 1)//
+                        {
+                            this.PrintSolution();
+                        }
+                        else Try(i + 1);
+                    }
+
+                }
+               
+
+            }
+        }
+        public void Solve() {
+            this.Try(0);
+        }
+        public void PrintSolution() {
+            for (int i = 0; i < K.Count<int>(); i++)
+                Console.Write(K[i]+" ");
+            Console.WriteLine();
+        }
+
+    }
+}
---
+++ Unfairness/Unfairness/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Unfairness
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] inc = {10,20,30,100,200,300,1000};
+            ExhaustedSearh ex = new ExhaustedSearh(inc, 3);
+            ex.Solve();
+            Console.ReadKey();
+        }
+    }
+}
